4ac440f215f82c8cd5202ff65815745926078e76
Hendrik Leidinger <hendrik.leidinger@nextcloud.com>
2026-01-21 09:49:58 -0800

    First attempt for own docker container with self compiled files, fails at loading fonts
    Signed-off-by: Hendrik Leidinger <hendrik.leidinger@nextcloud.com>

---
+++ build/build_files/documentserver-generate-allfonts.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+
+ONLYOFFICE_DATA_CONTAINER=false
+if [ "$1" != "" ]; then
+    ONLYOFFICE_DATA_CONTAINER=$1
+fi
+
+DIR="/var/www/onlyoffice/documentserver"
+
+export LD_LIBRARY_PATH=/var/www/onlyoffice/documentserver/server/FileConverter/bin:$LD_LIBRARY_PATH
+
+#Start generate AllFonts.js, font thumbnails and font_selection.bin
+echo -n Generating AllFonts.js, please wait...
+
+
+"$DIR/server/tools/allfontsgen"\
+  --input="$DIR/core-fonts"\
+  --allfonts-web="$DIR/sdkjs/common/AllFonts.js"\
+  --allfonts="$DIR/server/FileConverter/bin/AllFonts.js"\
+  --images="$DIR/sdkjs/common/Images"\
+  --selection="$DIR/server/FileConverter/bin/font_selection.bin"\
+  --output-web="$DIR/fonts"\
+  --use-system="true"\
+  --use-system-user-fonts="false"
+
+echo Done
+
+echo -n Generating presentation themes, please wait...
+"$DIR/server/tools/allthemesgen"\
+  --converter-dir="$DIR/server/FileConverter/bin"\
+  --src="$DIR/sdkjs/slide/themes"\
+  --output="$DIR/sdkjs/common/Images"
+
+"$DIR/server/tools/allthemesgen"\
+  --converter-dir="$DIR/server/FileConverter/bin"\
+  --src="$DIR/sdkjs/slide/themes"\
+  --output="$DIR/sdkjs/common/Images"\
+  --postfix="ios"\
+  --params="280,224"
+
+"$DIR/server/tools/allthemesgen"\
+  --converter-dir="$DIR/server/FileConverter/bin"\
+  --src="$DIR/sdkjs/slide/themes"\
+  --output="$DIR/sdkjs/common/Images"\
+  --postfix="android"\
+  --params="280,224"
+
+echo Done
+
+echo -n Generating js caches, please wait...
+"$DIR/server/FileConverter/bin/x2t" -create-js-cache
+
+echo Done
+
+# Setting user rights for files created in the previous steps
+chown -R ds:ds "$DIR/sdkjs"
+chown -R ds:ds "$DIR/server/FileConverter/bin"
+chown -R ds:ds "$DIR/fonts"
+
+#Remove gzipped files
+rm -f \
+  $DIR/fonts/*.gz \
+  $DIR/sdkjs/common/AllFonts.js.gz \
+  $DIR/sdkjs/common/Images/*.gz \
+  $DIR/sdkjs/slide/themes/themes.js.gz
+
+#Restart web-site and converter
+if [ "$ONLYOFFICE_DATA_CONTAINER" != "true" ]; then
+  if pgrep -x ""systemd"" >/dev/null; then
+    systemctl restart ds-docservice
+    systemctl restart ds-converter
+  elif pgrep -x ""supervisord"" >/dev/null; then
+    supervisorctl restart ds:docservice
+    supervisorctl restart ds:converter
+  fi
+  documentserver-flush-cache.sh
+fi
---
+++ build/build_files/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+service postgresql start
+service rabbitmq-server start
+service redis-server start
+service nginx start
+
+#tail -f /dev/null
+NODE_ENV=production-linux NODE_CONFIG_DIR=/etc/onlyoffice/documentserver NODE_DISABLE_COLORS=1 APPLICATION_NAME=ONLYOFFICE /var/www/onlyoffice/documentserver/server/FileConverter/converter &
+NODE_ENV=production-linux NODE_CONFIG_DIR=/etc/onlyoffice/documentserver NODE_DISABLE_COLORS=1 APPLICATION_NAME=ONLYOFFICE /var/www/onlyoffice/documentserver/server/DocService/docservice
+
